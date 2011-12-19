0725077c8fa58f7ed0082f94ed795c41be4de217
Michael <micxplosive@gmail.com>
2011-12-19 20:23:47 +0100

    Initial commit

---
+++ Pulse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using WhiteMagic.Internals;
+
+namespace D3DDetour
+{
+    public static class Pulse
+    {
+        private static D3DHook Hook;
+
+        public static void Initialize(D3DVersion ver)
+        {
+            switch (ver)
+            {
+                case D3DVersion.Direct3D9:
+                    Hook = new D3D9();
+                    break;
+                case D3DVersion.Direct3D10:
+                case D3DVersion.Direct3D10_1:
+                    Hook = new D3D10();
+                    break;
+                case D3DVersion.Direct3D11:
+                    Hook = new D3D11();
+                    break;
+            }
+
+            if (Hook == null)
+                throw new Exception("Hook = null!");
+
+            Hook.Initialize();
+        }
+    }
+}
